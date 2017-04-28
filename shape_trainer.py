d680e365b8b4e588d47fc2061fe64a200d15ca5a
buttegab <gabriel.butterick@students.olin.edu>
2017-04-28 16:10:40 -0400

    actually kinda works now

--- shape_trainer.py
+++ shape_trainer.py
@@ -6,31 +6,37 @@
 import cv2
 import numpy as np
 import math
+import sys
 
 def distance(endpoints, point):
-    """computes the perpendicular distance between a point a line defined by two endpoints"""
-    # print endpoints
-    start = endpoints[0][0]
-    end = endpoints[1][0]
-    # print start
-    # print end
-    dist_to_start = math.sqrt((point[0]-start[0])**2 + (point[1]-start[1])**2)
-    angle_of_big_line = math.atan2((end[1]-start[1]),(end[0]-start[0]))
-    angle_to_point = math.atan2((start[1]-point[1]),(start[0]-point[0]))
-    start_angle = angle_of_big_line-angle_to_point
-    perp_distance = dist_to_start*math.sin(start_angle)
-    return perp_distance
+    """
+    Computes the perpendicular distance between a point and a line defined by two endpoints.
+    Accepts a tuple of lists depicting x and y coordinates for the endpoints parameter and a list
+    containing the x and y coordinates of the desired point to compare to the line.
+    """
+    P1 = endpoints[0][0]
+    P2 = endpoints[1][0]
+    y0 = point[1]
+    x0 = point[0]
+    y1 = P1[1]
+    x1 = P1[0]
+    y2 = P2[1]
+    x2 = P2[0]
+
+    dist = abs((y2-y1)*x0-(x2-x1)*y0+x2*y1-y2*x1)/((y2-y1)**(2)+(x2-x1)**(2))**(0.5) 
+    return dist
 
 
 def dp(point_list, epsilon):
     max_distance = 0
-
+    # print len(point_list)
     furthest_point = None
     for i in range(len(point_list[1:-1])):
     	# print i[0][0]
     	# print(i,len(point_list))
+    	# print point_list
         dist = distance((point_list[0],point_list[-1]), point_list[i][0])
-        if dist>max_distance:
+        if dist>max_distance: #try adding a 'fudge factor' extra value to be sure its worth splitting for
             max_distance = dist
             furthest_point = i
     if max_distance > epsilon:
@@ -40,9 +46,14 @@ def dp(point_list, epsilon):
         result_list_2 = dp(new_line_2, epsilon)
         # print result_list_1
         # print result_list_2
-        if result_list_1:
-        	return result_list_1[0:-1].append(result_list_2)
+        if result_list_1 != None:
+        	# print "hey"
+        	# thing = result_list_1[0].append(result_list_2[0])
+        	thing = [np.append(result_list_1[0],result_list_2[0])]
+        	# print thing
+        	return thing
         else:
+        	# print "listen"
         	return result_list_2
     else:
         return [point_list[0], point_list[-1]]
@@ -52,7 +63,7 @@ def shape_trainer(trainImage):
 	minVal = 50
 	maxVal = 87
 	test_image = cv2.imread(trainImage,-1)
-	test_image = cv2.medianBlur(test_image,5)
+	# test_image = cv2.medianBlur(test_image,5)
 	edge_detected = cv2.Canny(test_image,minVal,maxVal)
 	contours, contour_hierarchy  = cv2.findContours(edge_detected, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
 	contour_image = cv2.drawContours(test_image, contours, -1, (0,255,0), 3)
@@ -65,19 +76,24 @@ def shape_trainer(trainImage):
 	res = []
 	vals = []
 	# print contours[0]
+	print len(contours)
 	for i in range(len(contours)):
-		temp = dp(contours[i], 100)
+		temp = dp(contours[i], 40)
 		# if temp:
-		# print temp
-		if temp:
-			res.append(temp)
-	return len(res)
+		# print len(temp[0])
+		# res.append(temp)
+		res.append(len(temp[0]))
+	# print res
+	# return len(res[0][0])/2
+	return res
 	# for x in res:
 		# vals.appe
 
 
 if __name__ == '__main__':
-	print shape_trainer("./circle_base.png")
+	sys.setrecursionlimit(2000)
+	# print shape_trainer("./circle_base.png")
 	# print shape_trainer("./multi_image.png")
-	print shape_trainer("./square_base.png")
-	print shape_trainer("./pentagon_base.png")
+	# print shape_trainer("./square_base.png") #works best at 177.9
+	print shape_trainer("./squares_base.png")
+	# print shape_trainer("./pentagon_base.png")
