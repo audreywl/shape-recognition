e97556dd3fbb29d205d336692d0e58a81baea23e
buttegab <gabriel.butterick@students.olin.edu>
2017-04-29 00:15:46 -0400

    I legitimately don't know why this works, and even then it's very sketchy, but its detecting edges* *it only works when rocco is here

--- edge_detector.py
+++ edge_detector.py
@@ -4,19 +4,19 @@ import math
 import sys
 def edge_detector(image):
 	"""
-	Maybe detects edges using numpy.
+	Given an input image file path, detects and returns the coordinates of each pixel detected as an edge.
 	"""
 	test_image = cv2.imread(image,-1)
 	width = len(test_image[0])
 	height = len(test_image)
-	tolerance = 20 #amount of pixel variation allowed before edge is detected
+	tolerance = 762 #amount of pixel variation allowed before edge is detected
 
 	#keep track of the coordinates (x,y) of pixels where a dramatic change in coloration is detected
 	down_pix = None
 	right_pix = None
 	pix_list = []
 	for y in range(len(test_image[0:-2])):
-		for x in range(len(test_image[0:-2])):
+		for x in range(len(test_image[0][0:-2])):
 			curr_pix = test_image[y][x]
 			if y+1 < len(test_image):
 				down_pix = test_image[y+1][x]
@@ -34,11 +34,43 @@ def edge_detector(image):
 
 			if diff_right >= tolerance or diff_down >= tolerance:
 				pix_list.append([x,y])
-	print pix_list
+	return create_image(pix_list,height,width)
+	# return pix_list
 
 
+def create_image(pix_list,height,width):
+	"""
+	Given a list of lists of coordinates, creates a numpy array of pixels that represent the detected edges of an image"
+	"""
+	# image = np.zeros((height,width))
+	image = np.ones((height,width,3))
+	image*=255
+	# image.fill([255,255,255])
+	# image.fill([])
 
+	# for y in range(len(image)):
+		# for x in range(len(image[0])):
+			# image[y][x] = [255,255,255]
+			# np.append(image[y][x],[255,255,255])
+			# np.put(image,[y,x],[255,255,255])
+
+	print image
+	for pix in pix_list:
+		y = pix[1]
+		x = pix[0]
+
+		image[x][y] = [0,0,0]
+		# np.put(image,[y,x],[0,0,0])
+	# print image
+	return image
 
 
 if __name__ == '__main__':
-	edge_detector("./square_base.png")
+	res = edge_detector("./square_base.png")
+	test_image = cv2.imread("./square_base.png",-1)
+	cv2.namedWindow( "Display window") # Create a window for display.
+	cv2.namedWindow( "Display window 2") # Create a window for display.
+	
+	cv2.imshow("Display window",res)
+	cv2.imshow("Display window 2",test_image)
+	cv2.waitKey(0)
